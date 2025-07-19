c6b8d1d1a9809bfb60ed7eb9bb884b88c6adccce
Kamil Myćka <49688601+myckakamil@users.noreply.github.com>
2025-07-19 20:41:36 +0200

    Created VM for testing purposes

--- terraform/homelab/main.tf
+++ terraform/homelab/main.tf
@@ -8,7 +8,7 @@ module "nextcloud" {
   target_node = "pve1"
 
   cores = 2
-  memory = 4096
+  memory = 8192
 
   disk_size = "200G"
   ip_config = "ip=172.30.20.101/24,gw=172.30.20.1"
@@ -127,11 +127,30 @@ module "mysql" {
   description = "Mariadb server"
   target_node = "pve2"
   cores = "1"
-  memory = "2038"
+  memory = 2048
   
   disk_size = "20G"
   ip_config = "ip=172.30.20.107/24,gw=172.30.20.1"
   vlan_tag = "20"
+
+  ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJF3mRlmUdCwWujN49vBX6n1cmp1CwEtqsYZf8eUftzt kamil"
+  cloudinit_password = var.secure_password
+}
+
+module "testing" {
+  source = "../proxmox_vm"
+
+  vm_id = "150"
+  vm_name = "testing.lan.mycka.net"
+  
+  description = "Server for testing various stuff"
+  target_node = "pve2"
+  cores = "1"
+  memory = "2048"
+  
+  disk_size = "20G"
+  ip_config = "ip=172.30.30.150/24,gw=172.30.30.1"
+  vlan_tag = "30"
 
   ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJF3mRlmUdCwWujN49vBX6n1cmp1CwEtqsYZf8eUftzt kamil"
   cloudinit_password = var.secure_password
